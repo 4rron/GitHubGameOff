423d240871c2cc54bfcc9287b3b8594930c186ea
4rron <air272@live.com>
2022-11-17 01:41:04 -0500

    Initial Upload

---
+++ Source/GitHubGameJam/Private/TransitionDummyClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TransitionDummyClass.h"
+
+TransitionDummyClass::TransitionDummyClass()
+{
+}
+
+TransitionDummyClass::~TransitionDummyClass()
+{
+}
